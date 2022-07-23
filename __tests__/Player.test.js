2b478d8700001210775f412011fbb7ec1cb2ae40
samvrny <samvrny@gmail.com>
2022-07-23 12:13:56 -0500

    added prototype to the Player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -13,6 +13,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
 
-    console.log(player, "Chicken");
+test('gets players stats as an object', () => {
+    const player = new Player('Bob');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Bob');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
